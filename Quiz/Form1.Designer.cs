026492a2d527814dcacc1d37152ff651a6837b65
Christos Seferiadis <1034123@johnabbottcollege.net>
2019-11-11 19:38:28 -0500

    Quiz2 commit

---
+++ Quiz/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Quiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtTelephone = new System.Windows.Forms.TextBox();
+            this.btnClearForm = new System.Windows.Forms.Button();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnCountUsers = new System.Windows.Forms.Button();
+            this.btnDeleteUsers = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(204, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            this.label1.Click += new System.EventHandler(this.btnRegister_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(204, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(204, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Telephone";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(384, 88);
+            this.txtFirstName.Multiline = true;
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(167, 26);
+            this.txtFirstName.TabIndex = 3;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(384, 156);
+            this.txtLastName.Multiline = true;
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(167, 26);
+            this.txtLastName.TabIndex = 4;
+            // 
+            // txtTelephone
+            // 
+            this.txtTelephone.Location = new System.Drawing.Point(384, 220);
+            this.txtTelephone.Multiline = true;
+            this.txtTelephone.Name = "txtTelephone";
+            this.txtTelephone.Size = new System.Drawing.Size(167, 26);
+            this.txtTelephone.TabIndex = 5;
+            // 
+            // btnClearForm
+            // 
+            this.btnClearForm.Location = new System.Drawing.Point(351, 295);
+            this.btnClearForm.Name = "btnClearForm";
+            this.btnClearForm.Size = new System.Drawing.Size(94, 40);
+            this.btnClearForm.TabIndex = 6;
+            this.btnClearForm.Text = "ClearForm";
+            this.btnClearForm.UseVisualStyleBackColor = true;
+            this.btnClearForm.Click += new System.EventHandler(this.btnClearForm_Click);
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(457, 295);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(94, 40);
+            this.btnRegister.TabIndex = 7;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click_1);
+            // 
+            // btnCountUsers
+            // 
+            this.btnCountUsers.Location = new System.Drawing.Point(351, 341);
+            this.btnCountUsers.Name = "btnCountUsers";
+            this.btnCountUsers.Size = new System.Drawing.Size(94, 40);
+            this.btnCountUsers.TabIndex = 8;
+            this.btnCountUsers.Text = "Count of Users";
+            this.btnCountUsers.UseVisualStyleBackColor = true;
+            this.btnCountUsers.Click += new System.EventHandler(this.btnCountUsers_Click);
+            // 
+            // btnDeleteUsers
+            // 
+            this.btnDeleteUsers.Location = new System.Drawing.Point(457, 341);
+            this.btnDeleteUsers.Name = "btnDeleteUsers";
+            this.btnDeleteUsers.Size = new System.Drawing.Size(94, 40);
+            this.btnDeleteUsers.TabIndex = 9;
+            this.btnDeleteUsers.Text = "Delete Users";
+            this.btnDeleteUsers.UseVisualStyleBackColor = true;
+            this.btnDeleteUsers.Click += new System.EventHandler(this.btnDeleteUsers_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDeleteUsers);
+            this.Controls.Add(this.btnCountUsers);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.btnClearForm);
+            this.Controls.Add(this.txtTelephone);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtTelephone;
+        private System.Windows.Forms.Button btnClearForm;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnCountUsers;
+        private System.Windows.Forms.Button btnDeleteUsers;
+    }
+}
+
---
+++ Quiz/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegister_Click_1(object sender, EventArgs e)
+        {
+
+            string fname = txtFirstName.Text;
+            string lname = txtLastName.Text;
+            string tel = txtTelephone.Text;
+
+
+            if (fname != "" && lname != "" && tel != "")
+            {
+                MessageBox.Show("First Name:" + fname + "\nLast Name:" + lname + "\nTelephone: " + tel, "Login Caption", MessageBoxButtons.OKCancel);
+                FileStream stream = new FileStream("C:/Users/1034123/Desktop/Net Programming/users.txt", FileMode.Append);
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    writer.Write(fname);
+                    writer.Write(lname);
+                    writer.Write(tel+ "\n");
+                }
+
+
+            }
+            else
+            {
+                MessageBox.Show("You must fill out the form");
+            }
+        }
+
+        private void btnClearForm_Click(object sender, EventArgs e)
+        {
+            string fname = txtFirstName.Text;
+            string lname = txtLastName.Text;
+            string tel = txtTelephone.Text;
+
+            txtFirstName.Text = string.Empty;
+            txtLastName.Text = string.Empty;
+            txtTelephone.Text = string.Empty;
+        }
+
+        private void btnCountUsers_Click(object sender, EventArgs e)
+      
+        {
+                using (StreamReader r = new StreamReader("C:/Users/1034123/Desktop/Net Programming/users.txt"))
+                {
+                    int i = 0;
+                    while (r.ReadLine() != null) { i++; }
+                    MessageBox.Show("The number of users are " + i);
+                }
+
+            
+        }
+
+        private void btnDeleteUsers_Click(object sender, EventArgs e)
+        {
+            string tempfile = Path.GetTempFileName();
+            FileStream stream = new FileStream("C:/Users/1034123/Desktop/Net Programming/users.txt", FileMode.Create);
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                File.Delete(tempfile);
+                MessageBox.Show("File has been deleted");
+            }
+
+            
+        }
+    }
+}
